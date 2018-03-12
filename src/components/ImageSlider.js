82c7258f81835318415bc6ba3429a94beab07fed
Vaibhav Patel <vp932712@gmail.com>
2018-03-12 17:11:59 -0400

    Done.

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,25 @@
 // Bomb Component Code Goes Here
+// In the components/ImageSlider.js file, create an ImageSlider React component.
+// Its initial state should have a property called currentSlideIndex that starts at 0.
+// It should only render out the text 'I am on slide <CURRENT_SLIDE>', where <CURRENT_SLIDE> is the value of this.state.currentSlideIndex.
+
+
+
+
+import React, { Component } from 'react';
+
+class ImageSlider extends Component {
+
+state = {
+  currentSlideIndex: 0
+    }
+
+
+  render() {
+    return (
+      <h1>I am on slide {this.state.currentSlideIndex}</h1>
+    )
+  }
+}
+
+export default ImageSlider;
